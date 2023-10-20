bb1eccfc48fb43bcd150f368654748ba56f2c2d4
StevenWeng60 <stevenweng60@gmail.com>
2023-10-20 10:33:47 -0500

    Added functionality to display switch button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import DisplaySwitch from './Miscellaneous/DisplaySwitch';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [checked, setChecked] = useState(true);
+  const label = { inputProps: {'aria-label': 'Switch demo'}};
+
+  const handleSwitchChange = (event) => {
+    setChecked(event.target.checked);
+  }
 
   return (
-    <>
+    <div className="projectRoot" style={{background: checked ? "grey" : "white"}}>
       <div className='websiteHeader'>
-        <h1>Steven's Portfolio</h1>
+        <h1 className='websiteTitle'>Steven's Portfolio</h1>
+        <div className='inlineBlock'>
+          <DisplaySwitch
+            onChange={handleSwitchChange} 
+            inputProps={{'aria-label': 'controlled'}} 
+          />
+        </div>
       </div>
-        <Button variant="contained">Hello world</Button>
-    </>
+      <Button variant="contained">Hello world</Button>
+    </div>
   )
 }
 
